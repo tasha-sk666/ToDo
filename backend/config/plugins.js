93255d6e207457a2c245e115785a6de5f67d3524
Konstantin <timohakt@yandex.ru>
2023-04-11 21:03:47 +0300

    remove attributes key from response object

---
+++ backend/config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+    'transformer': {
+        enabled: true,
+        config: {
+            responseTransforms: {
+                removeAttributesKey: true,
+                removeDataKey: true,
+            },
+        }
+    },
+});
