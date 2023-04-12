b2d470210e212f0d9748fbadf1bd8dfea86bdd4a
Konstantin <timohakt@yandex.ru>
2023-04-12 19:12:24 +0300

    add ThemeProvider

---
+++ frontend/src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import { ReactNode, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY } from '@/shared/const/localStorage';
+import { Theme } from '@/shared/const/theme';
+import { ThemeContext } from '@/shared/lib/context/ThemeContext';
+
+let defaultTheme = Theme.LIGHT;
+
+if (typeof window !== 'undefined') {
+    defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+}
+
+interface ThemeProviderProps {
+    initialTheme?: Theme;
+    children: ReactNode;
+}
+
+const ThemeProvider = (props: ThemeProviderProps) => {
+    const {
+        initialTheme,
+        children,
+    } = props;
+
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+
+    const defaultProps = useMemo(() => ({
+        theme,
+        setTheme,
+    }), [theme]);
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} className="app"/>
+    </ThemeProvider>
+  )
 }
